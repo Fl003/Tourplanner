fabf23b9620c52c248913e1fe98c96aaab480b9f
if23b288 <if23b288@technikum-wien.at>
2025-06-22 11:10:12 +0200

    belongs to last commit

--- src/main/java/org/example/tourplanner/model/Log.java
+++ src/main/java/org/example/tourplanner/model/Log.java
@@ -1,42 +1,75 @@
 package org.example.tourplanner.model;
 
 import javafx.beans.property.*;
+import net.bytebuddy.build.Plugin;
 
 import java.time.LocalDate;
 
 public class Log {
+    private final Long id;
+    private final LongProperty tourId;
     private final ObjectProperty<LocalDate> date;
     private final IntegerProperty hour;
     private final IntegerProperty minute;
     private final StringProperty comment;
-    private final StringProperty difficulty;
+    private final ObjectProperty<Difficulty> difficulty;
     private final IntegerProperty totalDistance;
     private final IntegerProperty totalTime;
     private final IntegerProperty rating;
 
-    public Log(LocalDate date, Integer hour, Integer minute, String comment, String difficulty, int totalDistance, int totalTime, int rating) {
+    public Log(Long id, Long tourId, LocalDate date, Integer hour, Integer minute, String comment, String difficulty, int totalDistance, int totalTime, int rating) {
+        this.id = id;
+        this.tourId = new SimpleLongProperty(tourId);
         this.date = new SimpleObjectProperty<>(date);
         this.hour = new SimpleIntegerProperty(hour);
         this.minute = new SimpleIntegerProperty(minute);
         this.comment = new SimpleStringProperty(comment);
-        this.difficulty = new SimpleStringProperty(difficulty);
+        this.difficulty = new SimpleObjectProperty<>(Difficulty.valueOf(difficulty));
+        this.totalDistance = new SimpleIntegerProperty(totalDistance);
+        this.totalTime = new SimpleIntegerProperty(totalTime);
+        this.rating = new SimpleIntegerProperty(rating);
+    }
+
+    public Log(Long tourId, LocalDate date, Integer hour, Integer minute, String comment, String difficulty, int totalDistance, int totalTime, int rating) {
+        this.id = null;
+        this.tourId = new SimpleLongProperty(tourId);
+        this.date = new SimpleObjectProperty<>(date);
+        this.hour = new SimpleIntegerProperty(hour);
+        this.minute = new SimpleIntegerProperty(minute);
+        this.comment = new SimpleStringProperty(comment);
+        this.difficulty = new SimpleObjectProperty<>(Difficulty.valueOf(difficulty));
         this.totalDistance = new SimpleIntegerProperty(totalDistance);
         this.totalTime = new SimpleIntegerProperty(totalTime);
         this.rating = new SimpleIntegerProperty(rating);
     }
 
     public Log() {
+        this.id = null;
+        this.tourId = new SimpleLongProperty(0L);
         this.date = new SimpleObjectProperty<>(LocalDate.now());
         this.hour = new SimpleIntegerProperty(0);
         this.minute = new SimpleIntegerProperty(0);
         this.comment = new SimpleStringProperty("");
-        this.difficulty = new SimpleStringProperty("");
+        this.difficulty = new SimpleObjectProperty<>(Difficulty.Easy);
         this.totalDistance = new SimpleIntegerProperty(0);
         this.totalTime = new SimpleIntegerProperty(0);
         this.rating = new SimpleIntegerProperty(0);
     }
 
-    public StringProperty difficultyProperty() { return difficulty; }
+    public Long getId() { return id; }
+    public Long getTourId() { return tourId.get(); }
+    public Difficulty getDifficulty() { return difficulty.get(); }
+    public Integer getTotalDistance() {
+        return totalDistance.get();
+    }
+    public Integer getTotalTime() { return totalTime.get(); }
+    public Integer getRating() { return rating.get(); }
+    public String getComment() { return comment.get(); }
+    public LocalDate getDate() { return date.get(); }
+    public Integer getHour() { return hour.get(); }
+    public Integer getMinute() { return minute.get(); }
+
+    public ObjectProperty<Difficulty> difficultyProperty() { return difficulty; }
     public IntegerProperty totalDistanceProperty() {
         return totalDistance;
     }
@@ -47,8 +80,9 @@ public class Log {
     public IntegerProperty hourProperty() { return hour; }
     public IntegerProperty minuteProperty() { return minute; }
 
+    public void setTourId(Long tourId) { this.tourId.set(tourId); }
     public void setDifficulty(String difficulty) {
-        this.difficulty.set(difficulty);
+        this.difficulty.set(Difficulty.valueOf(difficulty));
     }
     public void setTotalDistance(int totalDistance) {
         this.totalDistance.set(totalDistance);
